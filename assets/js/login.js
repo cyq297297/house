17d4b76ee101dfba6af48ae6a37de9e29d3ec931
chenwenrou <chenwenrou@itcast.cn>
2020-11-24 10:49:58 +0800

    完成登录注册

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,12 @@
-$(function() {
+$(function () {
   // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
+  $('#link_reg').on('click', function () {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
   // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
+  $('#link_login').on('click', function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
@@ -15,12 +15,14 @@ $(function() {
   var form = layui.form
   var layer = layui.layer
   // 通过 form.verify() 函数自定义校验规则
+
   form.verify({
     // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格~'],
     // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
+    repwd: function (value) {
+
+      // 通过形参(value)拿到的是确认密码框中的内容
       // 还需要拿到密码框中的内容
       // 然后进行一次等于的判断
       // 如果判断失败,则return一个提示消息即可
@@ -32,7 +34,7 @@ $(function() {
   })
 
   // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
+  $('#form_reg').on('submit', function (e) {
     // 1. 阻止默认的提交行为
     e.preventDefault()
     // 2. 发起Ajax的POST请求
@@ -40,18 +42,29 @@ $(function() {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('/api/reguser', data, function(res) {
+    $.post('/api/reguser', data, function (res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
       layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
+      // 清空注册表单
+      // $('#form_reg')[0].reset()
+      // 模拟人的点击行为 跳转到登录页面
       $('#link_login').click()
+      let uname = $('.reg-box [name=username]').val().trim()
+      $('.login-box [name=username]').val(uname)
+      let upawd = $('.reg-box [name=password]').val().trim()
+      $('.login-box [name=password]').val(upawd)
     })
+
   })
 
+
+
+
+
   // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
+  $('#form_login').submit(function (e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
@@ -59,7 +72,7 @@ $(function() {
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
@@ -68,6 +81,7 @@ $(function() {
         localStorage.setItem('token', res.token)
         // 跳转到后台主页
         location.href = '/index.html'
+
       }
     })
   })
